2377c1167f017b4eafa0b74a5b70adc5717ea0c0
yejineee <yygeniee@gmail.com>
2021-08-01 14:58:37 +0900

    Chore: inital setting

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/airbnb'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
