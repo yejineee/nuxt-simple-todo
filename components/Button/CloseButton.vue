6b9da401d64420f95c0f4f84b74a2142c74b9efd
yejineee <yygeniee@gmail.com>
2021-08-03 18:54:20 +0900

    Feat: 카드 클릭시 모달 열기, 닫기 클릭시 모달 닫기

---
+++ components/Button/CloseButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    x
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+
+})
+</script>
---
+++ components/Modal/DetailCardModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <Modal class='card-detail-modal'>
+    <div class='card__title'>{{title}}</div>
+    <button type="button" @click="closeHandler">닫기</button>
+  </Modal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    title:{
+      type: String,
+      required: true,
+    },
+    closeHandler:{
+      type: Function,
+      default(){
+        return () => {};
+      }
+    }
+  }
+})
+</script>
