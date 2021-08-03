5e8869d31a47a27ef54c452c751827bf14065ccc
yejineee <yygeniee@gmail.com>
2021-08-03 20:10:56 +0900

    Feat: 수정 버튼 클릭시 자동으로 인풋에 포커스하는 기능 추가

--- components/Modal/DetailCardModal.vue
+++ components/Modal/DetailCardModal.vue
@@ -4,18 +4,18 @@
       <button type="button" @click="closeHandler">닫기</button>
     </template>
     <form v-if="isEditMode" @submit="toggleEditMode">
-      <input  v-model="updatedTitle" class='edit-title-input' />
+      <input ref="inputRef" v-model="updatedTitle" class='edit-title-input' />
     </form>
-    <div v-else class='card__title' @click="toggleEditMode">{{updatedTitle}}</div>
+    <div v-else class='card__title' @click="onEditClickHandler">{{updatedTitle}}</div>
     <template #footer>
       <button v-if="isEditMode" type='button' @click="toggleEditMode">저장</button>
-      <button v-else type="button" @click="toggleEditMode">수정</button>
+      <button v-else type="button" @click="onEditClickHandler">수정</button>
     </template>
   </Modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, ref, nextTick, Ref } from '@vue/composition-api'
 
 export default defineComponent({
   props: {
@@ -34,7 +34,16 @@ export default defineComponent({
     const updatedTitle = ref(props.title);
     const isEditMode = ref(false);
     const toggleEditMode = () => isEditMode.value = !isEditMode.value;
+    const inputRef = ref(null);
+    const onEditClickHandler = () => {
+      toggleEditMode();
+      nextTick(() => {
+        inputRef.value.focus();
+      })
+    }
     return {
+      onEditClickHandler,
+      inputRef,
       updatedTitle,
       isEditMode,
       toggleEditMode,
