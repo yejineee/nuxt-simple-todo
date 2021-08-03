c996d84ec5600098600f995a5edb71b5fa17e32e
yejineee <yygeniee@gmail.com>
2021-08-03 18:31:10 +0900

    Feat: modal css 작성

---
+++ components/Modal/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="modal__background">
+    <div class="modal__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+
+})
+</script>
